c400967d85217f5c62e1deb7406dc25036ee86c2
AaryaBC <aaryabc49@gmail.com>
2017-04-15 23:04:58 -0400

    Added table view to each view controller

---
+++ dc_metro/dc_metro/MetroTimesCell.swift
@@ -0,0 +1,24 @@
+//
+//  MetroTimesCell.swift
+//  dc_metro
+//
+//  Created by Aarya BC on 4/15/17.
+//  Copyright © 2017 Aarya BC. All rights reserved.
+//
+
+import UIKit
+
+class MetroTimesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ dc_metro/dc_metro/MetroTimesViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MetroTimesViewController.swift
+//  dc_metro
+//
+//  Created by Aarya BC on 4/15/17.
+//  Copyright © 2017 Aarya BC. All rights reserved.
+//
+
+import UIKit
+
+class MetroTimesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.estimatedRowHeight = 150
+        tableView.rowHeight = UITableViewAutomaticDimension
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MetroTimesCell", for: indexPath) as! MetroTimesCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
